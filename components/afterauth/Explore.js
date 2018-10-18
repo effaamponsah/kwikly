32342303f4a780155c3a72dad3595e42fa0fbdbd
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2018-10-18 01:20:47 +0100

    code refactoring

--- components/afterauth/Explore.js
+++ components/afterauth/Explore.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Image, TouchableOpacity} from 'react-native';
+import React, { Component } from "react";
+import { Image, TouchableOpacity } from "react-native";
 import {
   Container,
   Header,
@@ -12,41 +12,36 @@ import {
   Left,
   Body,
   Icon
-} from 'native-base';
+} from "native-base";
 const cards = [
   {
-    text: 'Tasty Chef',
-    name: 'Salad',
-    image: require('../../assets/images/f1.png')
+    text: "Tasty Chef",
+    name: "Salad",
+    image: require("../../assets/images/f1.png")
   },
   {
-    text: 'McCoy',
-    name: 'Pizza',
-    image: require('../../assets/images/f2.png')
+    text: "McCoy",
+    name: "Pizza",
+    image: require("../../assets/images/f2.png")
   },
 
   {
-    text: 'Kappochino',
-    name: 'Pizza',
-    image: require('../../assets/images/f3.png')
+    text: "Kappochino",
+    name: "Pizza",
+    image: require("../../assets/images/f3.png")
   },
   {
-    text: 'KFC',
-    name: 'Chicken and Potato Chips',
-    image: require('../../assets/images/f4.png')
-  },
-
-  
-
-
+    text: "KFC",
+    name: "Chicken and Potato Chips",
+    image: require("../../assets/images/f4.png")
+  }
 ];
 export default class Explore extends React.Component {
-
   static navigationOptions = {
     tabBarIcon: ({ tintColor }) => (
       <Icon name="search" style={{ color: tintColor }} />
     )
-  } 
+  };
 
   render() {
     return (
@@ -54,36 +49,48 @@ export default class Explore extends React.Component {
         <View>
           <DeckSwiper
             dataSource={cards}
-            renderItem={item => <Card style={{
-            elevation: 3, height: 47
-            
-          }}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={item.image}/>
-                <Body>
-                <Text>{item.name}</Text>
-                    <Text note>{item.text}</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem cardBody>
-              <Image
+            renderItem={item => (
+              <Card
                 style={{
-                height: 350,
-                flex: 1
-              }}
-                source={item.image}/>
-            </CardItem>
-            <CardItem>
-                  <TouchableOpacity style={{flexDirection: 'row', flex: 1 }} onPress={console.log('order pressed')}>
-            <Icon name="cart" style={{
-              color: '#ED4A6A'
-            }}/>
-            <Text>Order</Text>
-            </TouchableOpacity >
-            </CardItem>
-          </Card>}/>
+                  elevation: 3,
+                  height: 47
+                }}
+              >
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={item.image} />
+                    <Body>
+                      <Text>{item.name}</Text>
+                      <Text note>{item.text}</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image
+                    style={{
+                      height: 350,
+                      flex: 1
+                    }}
+                    source={item.image}
+                  />
+                </CardItem>
+                <CardItem>
+                  <TouchableOpacity
+                    style={{ flexDirection: "row", flex: 1 }}
+                    onPress={console.log("order pressed")}
+                  >
+                    <Icon
+                      name="cart"
+                      style={{
+                        color: "#ED4A6A"
+                      }}
+                    />
+                    <Text>Order</Text>
+                  </TouchableOpacity>
+                </CardItem>
+              </Card>
+            )}
+          />
         </View>
       </Container>
     );
